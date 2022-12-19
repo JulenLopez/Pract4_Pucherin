17eeccef068c7934dbefa9bfe8cda7fc40ecb028
Ordenador de clase <julenlopezpanadero@gmail.com>
2022-12-19 10:14:05 +0100

    Cambios html y js

---
+++ js/pucherin.js
@@ -0,0 +1,8 @@
+// Modo texto
+window.onload = inicio;
+
+function inicio()
+{
+    let introduccion = document.getElementById("intro");
+    introduccion.innerHTML = "¿Cuántos jugadores vais a ser?";
+}
