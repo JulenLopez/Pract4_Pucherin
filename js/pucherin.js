656cf5126dd1fd3f0b3e82652bc2b8ba033dffb1
IreneZMS <115737802+IreneZMS@users.noreply.github.com>
2023-01-09 09:13:03 +0100

    fichero objetos

--- js/pucherin.js
+++ js/pucherin.js
@@ -1,8 +1,7 @@
 // Modo texto
 window.onload = inicio;
 
-function inicio()
-{
+function inicio(){
     let introduccion = document.getElementById("intro");
     introduccion.innerHTML = "¿Cuántos jugadores vais a ser?";
 }
