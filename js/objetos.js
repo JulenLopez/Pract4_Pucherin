2750c22e4fd28c2d115f6c61401be7f74f0204bc
IreneZMS <115737802+IreneZMS@users.noreply.github.com>
2023-01-13 10:11:05 +0100

    modified:   README.md 	modified:   js/objetos.js 	modified:   js/pucherin.js

--- js/objetos.js
+++ js/objetos.js
@@ -1,1 +1,15 @@
-var jugador = new Object(); 
+
+
+const jugador = {
+numeroJugador : 0,
+numeroFichas :  0,
+puntuacion : 0
+
+} 
+function Jugador(numeroJugador,numeroFichas,puntuacion){
+    this.numeroJugador=numeroJugador;
+    this.numeroFichas=numeroFichas;
+    this.puntuacion=puntuacion; 
+}
+
+
--- js/pucherin.js
+++ js/pucherin.js
@@ -5,3 +5,17 @@ function inicio(){
     let introduccion = document.getElementById("intro");
     introduccion.innerHTML = "¿Cuántos jugadores vais a ser?";
 }
+
+//array de jugadores que se rellena según el num selecionado, en el select? 
+const ARRAY_JUGADORES = [];
+for (let i = 1; i <= jugadores.value; i++) {
+  let jugador = Jugador("Jugador " + i,puntuacion);
+  array.push(jugador);
+  
+}
+
+function primerTurno(){
+    return Math.floor(Math.random() * (jugadores.value - 1 + 1) + 1)
+}
+
+
